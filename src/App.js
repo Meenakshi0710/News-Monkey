1c3d7d5af52be72ef977b3d03a454e01000a78a0
Meenakshi0710 <meenakshi.motiyani@gmail.com>
2022-02-23 11:54:00 +0530

    debug warnings

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import News from './Components/News';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
 
